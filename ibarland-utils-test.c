ce517d0f44c8896edd7c7c4f58373cc1cda44dcb
Ian Barland <ibarland@radford.edu>
2016-03-08 13:52:32 -0500

    Added tests for monus; converted some types to `natNum`, `stringConst` where not too painful.

--- ibarland-utils-test.c
+++ ibarland-utils-test.c
@@ -36,14 +36,17 @@ int main() {
     stringOnHeap = longToString(-3000000000L); testStr(stringOnHeap, "-3000000000" ); free(stringOnHeap);
     stringOnHeap = longToString( 9223372036854775807L); testStr(stringOnHeap,  "9223372036854775807" ); free(stringOnHeap);
     // ?? compiler-error?  I get a warning 'too big for long' if I try to use -9223372036854775808L, even though that ==LONG_MIN.
+    //   Ah -- I think it has to do with the fact that C doesn't have negative literals; 
+    //   it takes the positive and applies unary-minus, which may trigger implicit conversions (!) 
     stringOnHeap = longToString( LONG_MAX );   testStr(stringOnHeap,  "9223372036854775807" ); free(stringOnHeap);
     stringOnHeap = longToString( LONG_MIN );   testStr(stringOnHeap, "-9223372036854775808" ); free(stringOnHeap);
 
     
     testBool( time_usec() > 1451606400L*1000000L, true );  // > 2016 AD
     testBool( time_usec() < 3786912000L*1000000L, true );  // < 2090 AD
-    printf("\nCurrent time in ms is %ld. (2016=%ld, 2017=%ld).\n", time_usec(), 1451606400L*1000000L, 1483228800L*1000000L );
+    printTestMsg("\nCurrent time in ms is %ld. (2016=%ld, 2017=%ld).\n", time_usec(), 1451606400L*1000000L, 1483228800L*1000000L );
     
+    printTestMsg("\nTesting sgn, monus, mod: ");
     testInt( sgn(0), 0 );
     testInt( sgn(-0), 0 );
     testInt( sgn(0.0), 0 );
@@ -53,6 +56,22 @@ int main() {
     testInt( sgn(30), 1 );
     testInt( sgn(DBL_MIN), 1 );
     testInt( sgn(DBL_MAX), 1 );
+    
+    testInt( monus(7,3), 4 );
+    testInt( monus(3,7), 0 );
+    testInt( monus(7,7), 0 );
+    testInt( monus(0,0), 0 );
+    testInt( monus(0,1), 0 );
+    testInt( monus(1,0), 1 );
+    testInt( monus(1,1), 0 );
+
+    testNatNum( monus_u(7u,3u), 4u );
+    testNatNum( monus_u(3u,7u), 0u );
+    testNatNum( monus_u(7u,7u), 0u );
+    testNatNum( monus_u(0u,0u), 0u );
+    testNatNum( monus_u(0u,1u), 0u );
+    testNatNum( monus_u(1u,0u), 1u );
+    testNatNum( monus_u(1u,1u), 0u );
 
 
     testInt( modPos(0,3), 0 );
@@ -80,59 +99,83 @@ int main() {
     testInt( modPos(INT_MIN, -2), 0 );
     testInt( modPos(INT_MIN+1, -2), -1 );
 
-    testInt( lmodPos(0,3), 0 );
-    testInt( lmodPos(3,3), 0 );
-    testInt( lmodPos(6,3), 0 );
-    testInt( lmodPos(8,3), 2 );
-    testInt( lmodPos(INT_MAX,2), 1 );
-    testInt( lmodPos(INT_MAX-1, 2), 0 );
-    testInt( lmodPos(LONG_MAX,2), 1 );
-    testInt( lmodPos(LONG_MAX-1, 2), 0 );
+    testLong( lmodPos(0,3), 0 );
+    testLong( lmodPos(3,3), 0 );
+    testLong( lmodPos(6,3), 0 );
+    testLong( lmodPos(8,3), 2 );
+    testLong( lmodPos(INT_MAX,2), 1 );
+    testLong( lmodPos(INT_MAX-1, 2), 0 );
+    testLong( lmodPos(LONG_MAX,2), 1 );
+    testLong( lmodPos(LONG_MAX-1, 2), 0 );
 
-    testInt( lmodPos(-3,3), 0 );
-    testInt( lmodPos(-6,3), 0 );
-    testInt( lmodPos(-8,3), 1 );
-    testInt( lmodPos(INT_MIN, 2), 0 );
-    testInt( lmodPos(INT_MIN+1, 2), 1 );
-    testInt( lmodPos(LONG_MIN, 2), 0 );
-    testInt( lmodPos(LONG_MIN+1, 2), 1 );
+    testLong( lmodPos(-3,3), 0 );
+    testLong( lmodPos(-6,3), 0 );
+    testLong( lmodPos(-8,3), 1 );
+    testLong( lmodPos(INT_MIN, 2), 0 );
+    testLong( lmodPos(INT_MIN+1, 2), 1 );
+    testLong( lmodPos(LONG_MIN, 2), 0 );
+    testLong( lmodPos(LONG_MIN+1, 2), 1 );
 
-    testInt( lmodPos( 3,-3), 0 );
-    testInt( lmodPos( 6,-3), 0 );
-    testInt( lmodPos( 8,-3), -1 );
-    testInt( lmodPos(INT_MAX, -2), -1 );
-    testInt( lmodPos(INT_MAX-1, -2), 0 );
-    testInt( lmodPos(LONG_MAX, -2), -1 );
-    testInt( lmodPos(LONG_MAX-1, -2), 0 );
+    testLong( lmodPos( 3,-3), 0 );
+    testLong( lmodPos( 6,-3), 0 );
+    testLong( lmodPos( 8,-3), -1 );
+    testLong( lmodPos(INT_MAX, -2), -1 );
+    testLong( lmodPos(INT_MAX-1, -2), 0 );
+    testLong( lmodPos(LONG_MAX, -2), -1 );
+    testLong( lmodPos(LONG_MAX-1, -2), 0 );
 
-    testInt( lmodPos(-3,-3), 0 );
-    testInt( lmodPos(-6,-3), 0 );
-    testInt( lmodPos(-8,-3), -2 );
-    testInt( lmodPos(INT_MIN, -2), 0 );
-    testInt( lmodPos(INT_MIN+1, -2), -1 );
-    testInt( lmodPos(LONG_MIN, -2), 0 );
-    testInt( lmodPos(LONG_MIN+1, -2), -1 );
+    testLong( lmodPos(-3,-3), 0 );
+    testLong( lmodPos(-6,-3), 0 );
+    testLong( lmodPos(-8,-3), -2 );
+    testLong( lmodPos(INT_MIN, -2), 0 );
+    testLong( lmodPos(INT_MIN+1, -2), -1 );
+    testLong( lmodPos(LONG_MIN, -2), 0 );
+    testLong( lmodPos(LONG_MIN+1, -2), -1 );
 
 
-   testDouble( degToRad(0), 0 );
-   testDouble( degToRad(360), M_TAU );
-   testDouble( degToRad(180), M_PI );
-   testDouble( degToRad(30), M_PI/6 );
 
-   testDouble( radToDeg(0), 0.0 );
-   testDouble( radToDeg(M_TAU), 360.0 );
-   testDouble( radToDeg(M_PI),  180.0 );
-   testDouble( radToDeg(M_PI/6), 30.0 );
+    printTestMsg("\nTesting degToRad: ");
+    testDouble( degToRad(0), 0 );
+    testDouble( degToRad(360), M_TAU );
+    testDouble( degToRad(180), M_PI );
+    testDouble( degToRad(30), M_PI/6 );
+
+    testDouble( radToDeg(0), 0.0 );
+    testDouble( radToDeg(M_TAU), 360.0 );
+    testDouble( radToDeg(M_PI),  180.0 );
+    testDouble( radToDeg(M_PI/6), 30.0 );
    
-   testBool( approxEquals(2.0, 2.0), true );
-   testBool( approxEquals(2.0, 3.0), false );
-   testBool( approxEquals(2.0, 2.0e128), false );
-   testBool( approxEquals(2.0e128, 2.0000001e128), true );
-   testBool( approxEquals(2.0, 2.1), false );
-   testBool( approxEquals(2.0, 2.000000001), true );
+    printTestMsg("\nTesting approxEquals: ");
+    testBool( approxEquals(2.0, 2.0), true );
+     testBool( approxEquals(2.0, 3.0), false );
+     testBool( approxEquals(2.0, 2.000000001), true );
+    testBool( approxEquals(2.0, 2.0e128), false );
+    testBool( approxEquals(2.0e128, 2.000000001e128), true );
+    testBool( approxEquals(2.0, 2.1), false );
+    testBool( approxEquals(2.0, 2.000000001), true );
+    testBool( approxEquals(-0.0000001,+0.0000001), true );
 
-   testBool( approxEquals(NAN, NAN), false );
+    testBool( approxEquals( INFINITY, INFINITY), true );
+    testBool( approxEquals( INFINITY,-INFINITY), false );
+    testBool( approxEquals(-INFINITY, INFINITY), false );
+    testBool( approxEquals(-INFINITY,-INFINITY), true );
+    testBool( approxEquals( INFINITY, LONG_MAX), false );
+
+    testBool( approxEquals(1.0/INFINITY, 0.0), true );
+    testBool( approxEquals(0.0, 1.0/INFINITY), true );
+    testBool( approxEquals(-0.0, -1.0/INFINITY), true );
+    testBool( approxEquals(+0.0, -1.0/INFINITY), true );
+    testBool( approxEquals(+0.0, -0.0), true );
+    
+    
+    
+    testBool( approxEquals(NAN, NAN), false );
+    testBool( approxEquals(NAN, 17), false );
+    testBool( approxEquals(17, NAN), false );
+    testBool( approxEquals(INFINITY, NAN), false );
+    testBool( approxEquals(NAN, INFINITY), false );
 
     
+    printTestSummary();
     return 0;
     }
